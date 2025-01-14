68cb20689169009d3429b7f6291bf2bda48603b0
Daulet <daulet.tusupbaev@gmail.com>
2025-01-14 10:38:09 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# favorite_products_app
+
+Favorite Products App
---
+++ android/app/src/main/kotlin/com/example/favorite_products_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.favorite_products_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
